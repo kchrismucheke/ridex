cfbf729bf487a1000550e624f2ccad24c309e863
Chris Mucheke <kchrismucheke@gmail.com>
2020-06-11 20:51:46 +0300

    geohashing

---
+++ lib/ridex_web/controllers/ride_requests_controller.ex
@@ -0,0 +1,27 @@
+defmodule RidexWeb.RideRequestsController do
+  use RidexWeb, :controller
+
+  def create(
+        conn,
+        %{
+          "geohash" => geohash,
+          "position" => position
+        } = params
+      ) do
+    # Assuming we are authenticating user with a plug before and store it in conn.assigns
+    rider = conn.assigns[:current_user]
+
+    case Ridex.RideRequest.create(rider, position) do
+      {:ok, request} ->
+        RidexWeb.Endpoint.broadcast("cell:#{geohash}", "ride:requested", %{
+          request_id: request.id,
+          position: position
+        })
+
+        conn |> json(%{"request" => request})
+
+      {:error, reason} ->
+        conn |> json(%{"error" => "Unable to request a ride"})
+    end
+  end
+end
